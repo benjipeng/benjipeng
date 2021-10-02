b2a9cb9238efbdff6f610330a5ec7b88662f3bdc
Benji P <baijiepeng@gmail.com>
2021-10-02 15:44:08 -0400

    mostly done

--- src/package/components/cards/cards_types/projects/project_dialog/project_dialog_content_description/project_dialog_content_description_styles.js
+++ src/package/components/cards/cards_types/projects/project_dialog/project_dialog_content_description/project_dialog_content_description_styles.js
@@ -11,7 +11,7 @@ export const styles = (theme) => {
         }),
         typography: {
             whiteSpace: 'pre-line',
-            lineHeight: '24px'
+            lineHeight: '26px'
         }
     };
 };
