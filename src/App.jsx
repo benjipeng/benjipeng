416543553194b2843d11a6b0474d6c02eda8f54c
Benji P <baijiepeng@gmail.com>
2022-02-03 18:52:53 -0500

    updated Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ import { ReactComponent as SaveIcon } from './package/assets/icons/drop_file.svg
 
 import { styles } from './app_styles';
 
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
-const TRACKING_ID = 'UA-219172152-1'; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+// const TRACKING_ID = 'UA-219172152-1'; // YOUR_OWN_TRACKING_ID
+// ReactGA.initialize(TRACKING_ID);
 
 const useStyles = createUseStyles(styles);
 const mergeFunction = (objValue, srcValue) => {
