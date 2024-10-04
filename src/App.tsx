3f13d24dccb939cf89e4433b8fcd9f2300cfeb55
B. P. Hon <benji.h.peng@gmail.com>
2024-10-04 10:02:29 -0400

    new start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to Benji Peng's Portfolio
+      </h1>
+    </div>
+  );
+}
+
+export default App;
